8b9a8398af87cf1151c43a72e35803ad81033e15
definfo <hjsdbb1@gmail.com>
2025-07-14 22:25:51 +0800

    fix(nix): resolve dynamic linking issues for XiangShan

--- flake.nix
+++ flake.nix
@@ -5,76 +5,97 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        # === tool ===
-        wget
-        git
-        tmux
-        curl
-        time
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfreePredicate =
+          pkg:
+          builtins.elem (nixpkgs.lib.getName pkg) [
+            "espresso"
+          ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # === tool ===
+          wget
+          git
+          tmux
+          curl
+          time
 
-        # === runtime ===
-        python3
-        python3Packages.psutil # for XiangShan/scripts/xiangshan.py
-        openjdk
+          # === runtime ===
+          python3
+          python3Packages.psutil # for XiangShan/scripts/xiangshan.py
+          # openjdk
+          graalvmPackages.graalvm-ce
 
-        # === toolchain ===
-        gcc # host toolchain
-        pkgsCross.riscv64.buildPackages.gcc # riscv64-unknown-elf-xxx toolchain
-        llvm # for pgo
-        clang
-        gnumake # make
-        dtc # device tree compiler
-        flex
-        autoconf
-        bison
-        # override mill & verilator to use our version
-        (mill.overrideAttrs (finalAttrs: previousAttrs: {
-          version = "0.12.15";
-          src = fetchurl {
-            url = "https://repo1.maven.org/maven2/com/lihaoyi/mill-dist/${finalAttrs.version}/mill-dist-${finalAttrs.version}.exe";
-            hash = "sha256-6hu6AeIg9M4guzMyR9JUor+bhlVMEMPX1+FmQewKdtg=";
-          };
-        }))
-        (verilator.overrideAttrs (finalAttrs: previousAttrs: {
-          version = "5.028";
-          VERILATOR_SRC_VERSION = "v${finalAttrs.version}";
-          src = fetchFromGitHub {
-            owner = "verilator";
-            repo = "verilator";
-            rev = "v${finalAttrs.version}";
-            hash = "sha256-YgK60fAYG5575uiWmbCODqNZMbRfFdOVcJXz5h5TLuE=";
-          };
-          doCheck = false;
-        }))
+          # === toolchain ===
+          gcc # host toolchain
+          pkgsCross.riscv64.buildPackages.gcc # riscv64-unknown-elf-xxx toolchain
+          llvm # for pgo
+          clang
+          gnumake # make
+          dtc # device tree compiler
+          flex
+          autoconf
+          bison
+          # override mill & verilator to use our version
+          (mill.overrideAttrs (
+            finalAttrs: previousAttrs: {
+              version = "0.12.15";
+              src = fetchurl {
+                url = "https://repo1.maven.org/maven2/com/lihaoyi/mill-dist/${finalAttrs.version}/mill-dist-${finalAttrs.version}.exe";
+                hash = "sha256-6hu6AeIg9M4guzMyR9JUor+bhlVMEMPX1+FmQewKdtg=";
+              };
+            }
+          ))
+          (verilator.overrideAttrs (
+            finalAttrs: previousAttrs: {
+              version = "5.028";
+              VERILATOR_SRC_VERSION = "v${finalAttrs.version}";
+              src = fetchFromGitHub {
+                owner = "verilator";
+                repo = "verilator";
+                rev = "v${finalAttrs.version}";
+                hash = "sha256-YgK60fAYG5575uiWmbCODqNZMbRfFdOVcJXz5h5TLuE=";
+              };
+              doCheck = false;
+            }
+          ))
 
-        # === debug ===
-        gtkwave
+          # === sim ===
+          espresso # ! Unfree
+          (callPackage ./nix/circt.nix { })
 
-        # === lib ===
-        readline
-        SDL2
-        zlib
-        zstd
-        sqlite
-      ];
-      shellHook = ''
-        echo "=== Welcome to XiangShan devshell! ==="
-        echo "Version info:"
-        echo "- $(verilator --version)"
-        echo "- $(mill --version | head -n 1)"
-        echo "- $(gcc --version | head -n 1)"
-        echo "- $(riscv64-unknown-linux-gnu-gcc --version | head -n 1)"
-        echo "- $(java -version 2>&1 | head -n 1)"
-        echo "You can press Ctrl + D to exit devshell."
-        export LD_LIBRARY_PATH="${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
-        source $(pwd)/env.sh
-      '';
+          # === debug ===
+          gtkwave
+
+          # === lib ===
+          readline
+          SDL2
+          zlib
+          zstd
+          sqlite
+        ];
+        shellHook = ''
+          echo "=== Welcome to XiangShan devshell! ==="
+          echo "Version info:"
+          echo "- $(verilator --version)"
+          echo "- $(mill --version | head -n 1)"
+          echo "- $(gcc --version | head -n 1)"
+          echo "- $(riscv64-unknown-linux-gnu-gcc --version | head -n 1)"
+          echo "- $(java -version 2>&1 | head -n 1)"
+          echo "You can press Ctrl + D to exit devshell."
+
+          source ./env.sh
+          ln -sf $(which espresso) ./XiangShan/src/main/resources/espresso
+          ln -sf $(which firtool) $HOME/.cache/llvm-firtool/1.62.1/bin/firtool
+        '';
+      };
     };
-  };
 }
---
+++ nix/circt-llvm.nix
@@ -0,0 +1,80 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  ninja,
+  fetchFromGitHub,
+  llvm,
+  python3,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "circt-llvm";
+  version = "1.62.1";
+  src = fetchFromGitHub {
+    owner = "llvm";
+    repo = "circt";
+    tag = "firtool-${finalAttrs.version}";
+    hash = "sha256-hLPGfoXJwuaCYcrSvRCm5ob+aJdP/XsfAVyf1aB27wc=";
+    fetchSubmodules = true;
+  };
+
+  requiredSystemFeatures = [ "big-parallel" ];
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    python3
+  ];
+
+  preConfigure = ''
+    cd llvm/llvm
+  '';
+
+  cmakeFlags = [
+    "-DBUILD_SHARED_LIBS=ON"
+    "-DLLVM_ENABLE_BINDINGS=OFF"
+    "-DLLVM_ENABLE_OCAMLDOC=OFF"
+    "-DLLVM_BUILD_EXAMPLES=OFF"
+    "-DLLVM_OPTIMIZED_TABLEGEN=ON"
+    "-DLLVM_ENABLE_PROJECTS=mlir"
+    "-DLLVM_TARGETS_TO_BUILD="
+
+    # This option is needed to install llvm-config
+    "-DLLVM_INSTALL_UTILS=ON"
+  ];
+
+  outputs = [
+    "out"
+    "lib"
+    "dev"
+  ];
+
+  postInstall = ''
+    # move llvm-config to $dev to resolve a circular dependency
+    moveToOutput "bin/llvm-config*" "$dev"
+
+    # move all lib files to $lib except lib/cmake
+    moveToOutput "lib" "$lib"
+    moveToOutput "lib/cmake" "$dev"
+
+    # patch configuration files so each path points to the new $lib or $dev paths
+    substituteInPlace "$dev/lib/cmake/llvm/LLVMConfig.cmake" \
+      --replace 'set(LLVM_BINARY_DIR "''${LLVM_INSTALL_PREFIX}")' 'set(LLVM_BINARY_DIR "'"$lib"'")'
+    substituteInPlace \
+      "$dev/lib/cmake/llvm/LLVMExports-release.cmake" \
+      "$dev/lib/cmake/mlir/MLIRTargets-release.cmake" \
+      --replace "\''${_IMPORT_PREFIX}/lib/lib" "$lib/lib/lib" \
+      --replace "\''${_IMPORT_PREFIX}/lib/objects-Release" "$lib/lib/objects-Release" \
+      --replace "$out/bin/llvm-config" "$dev/bin/llvm-config" # patch path for llvm-config
+  '';
+
+  doCheck = true;
+  checkTarget = "check-mlir";
+
+  meta = llvm.meta // {
+    maintainers = with lib.maintainers; [
+      sharzy
+      pineapplehunter
+    ];
+  };
+})
